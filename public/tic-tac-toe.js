214b7a428344b59f3accdbb1d6a57b0e5cfde493
sido <imbilusydney09@gmail.com>
2024-07-27 09:29:37 +0300

    implemented game logic

--- public/tic-tac-toe.js
+++ public/tic-tac-toe.js
@@ -3,27 +3,69 @@ document.addEventListener("DOMContentLoaded", event => {
     const O_IMG_SRC = './images/o.png';  
 
     const cells = document.querySelectorAll('[data-cell]');
+    const newGameButton = document.getElementById('new-game');
+    const gameStatus = document.getElementById('gameStatus');
     let isXTurn = true;
+    let board = Array(9).fill(null);
 
-    cells.forEach(cell => {
-        cell.addEventListener('click', handleClick, { once: true });
+    // A 2d array with the index positions for all possible winning combos
+    const WINNING_COMBINATIONS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+
+    cells.forEach((cell, index) => {
+        cell.addEventListener('click', () => handleClick(cell, index), { once: true });
     });
 
-    function handleClick(e) {
-        const cell = e.target;
+    function handleClick(cell, index) {
         const currentClass = isXTurn ? X_IMG_SRC : O_IMG_SRC;
-        placeMark(cell, currentClass);
-        swapTurns();
+        placeMark(cell, currentClass, index);
+        if (checkWin(currentClass)) {
+            endGame(false);
+        } else if (isTie()) {
+            endGame(true);
+        } else {
+            swapTurns();
+        }
     }
 
-    function placeMark(cell, currentClass) {
+    function placeMark(cell, currentClass, index) {
         const img = document.createElement('img');
         img.src = currentClass;
         cell.appendChild(img);
+        board[index] = currentClass;
     }
 
     function swapTurns() {
         isXTurn = !isXTurn;
+    }
+
+    function checkWin(currentClass) {
+        return WINNING_COMBINATIONS.some(combination => {
+            return combination.every(index => {
+                return board[index] === currentClass;
+            });
+        });
+    }
+
+    function isTie() {
+        return board.every(cell => cell !== null);
+    }
+
+    function endGame(tie) {
+        if (tie) {
+            gameStatus.innerText = 'Winner: None';
+        } else {
+            gameStatus.innerText = `Winner: ${isXTurn ? 'X' : 'O'}`;
+        }
+        cells.forEach(cell => cell.removeEventListener('click', handleClick));
     }
 
 })
